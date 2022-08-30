053fc208425c3326b31156507460b767f9d2b768
renthog <58467174+renthog@users.noreply.github.com>
2022-08-30 11:15:45 -0700

    undo move

--- api/packages/api/src/routes/index.ts
+++
@@ -1,3 +0,0 @@
-export { default as apiV1Routes } from './api-v1';
-export { default as coinMarketCapV1Routes } from './external/coinmarketcap-v1';
-export { makeTvRouter } from './tv';
--- api/packages/api/src/types/util.ts
+++
@@ -1,4 +0,0 @@
-/**
- * Force all properties of a type T to be V.
- */
-export type ForceProperties<T, V> = { [x in keyof T]: V };
