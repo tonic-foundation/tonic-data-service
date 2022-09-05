59c16c7682c85cccda74f3079cc03ae824266e14
renthog <58467174+renthog@users.noreply.github.com>
2022-09-04 17:05:34 -0700

    add rewards program stats

--- api/packages/api/src/routes/rewards/index.ts
+++ api/packages/api/src/routes/rewards/index.ts
@@ -2,11 +2,13 @@
 import { FastifyInstance } from 'fastify';
 
 import history from './history';
+import stats from './stats';
 import unfinalized from './unfinalized';
 
 export default function registerRewardsRoutes(api: FastifyInstance, _: unknown, done: () => unknown) {
   api.register(unfinalized);
   api.register(history);
+  api.register(stats);
 
   done();
 }
