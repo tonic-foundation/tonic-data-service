6f3f44102b1bdef54f6769306af93df014510177
renthog <58467174+renthog@users.noreply.github.com>
2022-10-11 12:17:42 -0700

    add missing migration

--- indexer/migrations/2022-10-08-222557_add-token-denomination/down.sql
+++ indexer/migrations/2022-10-08-222557_add-token-denomination/down.sql
@@ -1,2 +1,9 @@
 -- This file should undo anything in `up.sql`
+begin
+;
+
 drop view nep_141_token_denomination;
+
+drop view market_with_denomination;
+
+end;
--- indexer/migrations/2022-10-08-222557_add-token-denomination/up.sql
+++ indexer/migrations/2022-10-08-222557_add-token-denomination/up.sql
@@ -10,4 +10,15 @@ create view nep_141_token_denomination as (
         nep_141_token
 );
 
+create view market_with_denomination as (
+    select
+        m. *,
+        bt.denomination base_denomination,
+        qt.denomination quote_denomination
+    from
+        market m
+        join nep_141_token_denomination bt on m.base_token_id = bt.id
+        join nep_141_token_denomination qt on m.quote_token_id = qt.id
+);
+
 end;
