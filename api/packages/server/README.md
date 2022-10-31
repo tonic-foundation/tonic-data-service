28da0b4daf91022a4ee93523080c710f4be862c1
renthog <58467174+renthog@users.noreply.github.com>
2022-10-30 22:25:51 -0700

    idk

---
+++ api/packages/server/README.md
@@ -0,0 +1,5 @@
+# Running an incentives program
+
+- Run `./scripts/rewards/init-program.ts`
+  - if you need to extend the program, you may need to run in the
+    script manually
---
+++ api/packages/server/src/routes/rewards/v3/index.ts
@@ -0,0 +1,23 @@
+// TODO: unimplemented
+// Routes for USN rewards program.
+import { FastifyInstance } from 'fastify';
+import eligibility from './eligibility';
+
+import history from './history';
+// import payouts from './payouts';
+import parameters from './parameters';
+import stats from './stats';
+import unfinalized from './unfinalized';
+import leaderboard from './leaderboard';
+
+export default function registerRewardsV3Routes(api: FastifyInstance, _: unknown, done: () => unknown) {
+  api.register(eligibility);
+  api.register(history);
+  api.register(leaderboard);
+  api.register(parameters);
+  // api.register(payouts);
+  api.register(stats);
+  api.register(unfinalized);
+
+  done();
+}
