ee2f95fe1c3050d121d8b7d5a02b2c393f896974
renthog <58467174+renthog@users.noreply.github.com>
2023-01-17 00:18:13 -0800

    add leaderboard v4 routes

--- api/packages/server/src/routes/rewards/index.ts
+++ api/packages/server/src/routes/rewards/index.ts
@@ -1,3 +1,4 @@
 export { default as rewardsV1Routes } from './v1';
 export { default as rewardsV2Routes } from './v2';
 export { default as rewardsV3Routes } from './v3';
+export { default as rewardsV4Routes } from './v4';
